d641150d9fb856c752b302c96a33db0c34c635a5
ElliottWery <92320166+ElliottWery@users.noreply.github.com>
2022-04-11 14:12:42 -0700

    changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+const sayHello = (req, res) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
+  };
+
+  const saySomething = (req, res) => {
+    const greeting = req.params.greeting;
+    const content = `${greeting}!`;
+    res.send(content);
+  };
+  
+app.get("/say/:greeting", saySomething);
+app.use(morgan("dev"));
+app.get("/hello", sayHello);
 
 module.exports = app;
